319d8c65c9a402c384b9ae812ae06928f1e657bb
Daniel Ronalds <nzdanron@gmail.com>
2025-12-21 11:57:28 +1300

    feat: add pager support for large outputs

--- internal/config.go
+++ internal/config.go
@@ -3,3 +3,6 @@ package internal
 const CONFIG_NAME = "clint.yaml"
 
 const VERSION = "0.1.0"
+
+const MAX_LINES_WITHOUT_PAGER = 20
+const FALLBACK_PAGER = "less"
--- internal/pipelines/presentation.go
+++ internal/pipelines/presentation.go
@@ -2,7 +2,11 @@ package pipelines
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/danielronalds/clint/internal"
 	a "github.com/logrusorgru/aurora/v4"
 )
 
@@ -15,5 +19,28 @@ func printFailBadge(stepName string) {
 }
 
 func printFailedOutput(output string) {
-	fmt.Printf("\n%v\n", output)
+	linesInOutput := strings.Split(output, "\n")
+
+	if len(linesInOutput) < internal.MAX_LINES_WITHOUT_PAGER {
+		fmt.Printf("\n%s\n", output)
+		return
+	}
+
+	pager, isSet := os.LookupEnv("PAGER")
+
+	if !isSet {
+		pager = internal.FALLBACK_PAGER
+	}
+
+	cmd := exec.Command(pager)
+	if !isSet {
+		cmd = exec.Command(pager, "-R")
+	}
+	cmd.Stdin = strings.NewReader(output)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("\n%s\n", output)
+	}
 }
